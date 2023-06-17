ec9557b2bf4e8e303b91aade84a5db3a1f69009c
adoestreito <adestreito@fastly.com>
2023-06-17 15:30:28 +0200

    first commit

---
+++ target/debug/deps/hello_world-b987f4fc421725d7.d
@@ -0,0 +1,5 @@
+/Users/andresdoestreito/Documents/Fastly/InternalProjects/hello_world/target/debug/deps/hello_world-b987f4fc421725d7: src/main.rs
+
+/Users/andresdoestreito/Documents/Fastly/InternalProjects/hello_world/target/debug/deps/hello_world-b987f4fc421725d7.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/Users/andresdoestreito/Documents/Fastly/InternalProjects/hello_world/target/debug/hello_world: /Users/andresdoestreito/Documents/Fastly/InternalProjects/hello_world/src/main.rs
